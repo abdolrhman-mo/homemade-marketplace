645fe8f2927de0626ba6a3030e33fbdd71532668
abdolrhman-mo <abdelrhmanlearn@gmail.com>
2026-05-03 22:27:22 +0300

    Fix User pre-save hook for Mongoose 9 + ignore local mongod data

--- models/mongoose/User.js
+++ models/mongoose/User.js
@@ -20,11 +20,10 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function() {
     if (this.isModified('password') && !this.password.startsWith('$2b$')) {
         this.password = await bcrypt.hash(this.password, 10);
     }
-    next();
 });
 
 // Validate password
